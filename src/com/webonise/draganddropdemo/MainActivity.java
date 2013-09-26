d642376d8c991332e6f517f065b1a7363d3c42dc
ranvijaySingh-Webonise <rana.ranvijay@weboniselab.com>
2013-09-26 12:45:29 +0530

    S3 :InitializeComponents() created

--- src/com/webonise/draganddropdemo/MainActivity.java
+++ src/com/webonise/draganddropdemo/MainActivity.java
@@ -1,22 +1,20 @@
 package com.webonise.draganddropdemo;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
+	ImageView imageView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initializeComponents();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private void initializeComponents() {
+		imageView=(ImageView)findViewById(R.id.imageViewDrag);
 	}
-
 }
